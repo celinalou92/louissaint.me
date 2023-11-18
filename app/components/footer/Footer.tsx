b4532364bae4edcdbd852d017591263e3a67300f
Celina Louissaint <celinalouissaint@gmail.com>
2023-11-18 10:50:28 -0800

    Add footer and contact info

---
+++ app/components/footer/Footer.tsx
@@ -0,0 +1,57 @@
+import { Box, Button, Stack, Typography, styled } from "@mui/material";
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+
+
+export const Footer = () => {
+    const contactData = [
+        {
+            method: "Email",
+            linkText:"celinalouissaint@gmail.com",
+            link: "https://celinalouissaint@gmail.com",
+            icon: <MailOutlineIcon  />,
+        },
+        {
+            method: "Github",
+            linkText:"github.com/celinalou92",
+            link: "https://github.com/celinalou92",
+            icon: <GitHubIcon />,
+        },
+        {
+            method: "LinkedIn",
+            linkText:"linkedin.com/in/louissaint-c",
+            link: "https://linkedin.com/in/louissaint-c",
+            icon: <LinkedInIcon />,
+        },
+    ]
+    return (
+        <Box sx={{ bgcolor: 'background.default', p: 8 }} component="footer">
+            <Typography variant="h5" align="left" margin="1em auto">
+                Contact Celina
+            </Typography>
+            {contactData.map((contact) => (
+                <Stack key={contact.method} display="flex" direction={"row"}  alignItems="center" width="100%">
+ 
+                    <Button
+                    href={contact.link} 
+                    target="_blank"
+                    startIcon={contact.icon}
+                    size="large"
+                    fullWidth
+                    >
+                        {contact.method}:
+                    </Button>
+                    <Button 
+                    href={contact.link} 
+                    target="_blank"
+
+                    color="secondary"
+                    >
+                        {contact.linkText}
+                    </Button>
+                </Stack>
+            ))}
+        </Box>
+    );
+};      
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 /* eslint-disable react/no-unescaped-entities */
 import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
 import { SkillList } from './components/skillList/SkillsList';
 import { Hero } from './components/hero/Hero';
 import { NavBar } from './components/navBar/Navbar';
 import { Projects } from './components/projects/Projects';
 import projectData from "../data/projectsData"
 import { skillsListData } from '@/data/skillListData';
+import { Footer } from './components/footer/Footer';
 
 
 
@@ -26,19 +26,7 @@ export default function Home() {
         <SkillList skillsListData={skillsListData} />
         <Projects projects={projectData} />
       </Box>
-        {/* Footer */}
-        <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
-          <Typography variant="body1" align="center" gutterBottom>
-            Footer
-          </Typography>
-          <Typography
-            variant="subtitle1"
-            align="center"
-          >
-            Something here to give the footer a purpose!
-          </Typography>
-        </Box>
-        {/* End footer */}
+      <Footer />
     </>
   )
 }
