458782da459edaa7c2eb8ba12de0fd2c9281bddf
Celina <celinalouissaint@gmail.com>
2024-06-28 11:21:43 -0700

    finish skillset styles

--- app/components/skillList/SkillsList.tsx
+++ app/components/skillList/SkillsList.tsx
@@ -15,13 +15,14 @@ export const SkillList = ({ skillsListData }: { skillsListData: Skills }) => {
 
   return (
     <Stack
-    id="about"
-    height="100vh"
-    display={"flex"}
-    flexDirection={"column"}
-    alignItems={"center"}
-    justifyContent={"center"}
-  >
+      id="about"
+      height="100vh"
+      display={"flex"}
+      flexDirection={"column"}
+      alignItems={"center"}
+      paddingTop={"20%"}
+    >
+      <Stack maxWidth={"500px"} maxHeight={"500px"}>
         <Typography
           sx={{ letterSpacing: ".2rem", borderBottom: ".5px solid #D6A23A" }}
           variant="h3"
@@ -37,63 +38,119 @@ export const SkillList = ({ skillsListData }: { skillsListData: Skills }) => {
           flexDirection="row"
           alignItems="flex-start"
           justifyContent="space-between"
-          spacing={3}
-          padding={2}
+          spacing={2}
+          rowGap={5}
         >
-          <Grid item display="flex" flexDirection="column">
-            <Typography gutterBottom variant="h5">
-              Certifications
-            </Typography>
-            <Divider />
-            <Typography gutterBottom variant="body1">
-              {certifications[0].name}
-            </Typography>
-          </Grid>
-          <Grid item display="flex" flexDirection="column">
-            <Typography gutterBottom variant="h5">
+          <Grid
+            item
+            display="flex"
+            flexDirection="column"
+            width={"230px"}
+            height={"180px"}
+          >
+            <Typography
+              gutterBottom
+              variant="h6"
+              sx={{
+                borderBottom: ".5px solid #D6A23A",
+              }}
+            >
               Programming Languages
             </Typography>
-            <Divider />
             {programming_languages.map((pl: Skill) => (
               <Typography key={pl.name} gutterBottom variant="body1">
                 {pl.name}
               </Typography>
             ))}
           </Grid>
-          <Grid item display="flex" flexDirection="column">
-            <Typography gutterBottom variant="h5">
-              Frameworks & Libraries
-            </Typography>
-            <Divider />
-            {frameworks_libraries.map((fw: Skill) => (
-              <Typography key={fw.name} gutterBottom variant="body1">
-                {fw.name}
-              </Typography>
-            ))}
-          </Grid>
-          <Grid item display="flex" flexDirection="column">
-            <Typography gutterBottom variant="h5">
+          <Grid
+            item
+            display="flex"
+            flexDirection="column"
+            width={"230px"}
+            maxHeight={"180px"}
+          >
+            <Typography
+              gutterBottom
+              variant="h6"
+              sx={{
+                borderBottom: ".5px solid #D6A23A",
+              }}
+            >
               Methodologies
             </Typography>
-            <Divider />
             {methodologies.map((m: Skill) => (
               <Typography key={m.name} gutterBottom variant="body1">
                 {m.name}
               </Typography>
             ))}
           </Grid>
-          <Grid item display="flex" flexDirection="column">
-            <Typography gutterBottom variant="h5">
+          <Grid
+            item
+            display="flex"
+            flexDirection="column"
+            width={"230px"}
+            height={"180px"}
+          >
+            <Typography
+              gutterBottom
+              variant="h6"
+              sx={{
+                borderBottom: ".5px solid #D6A23A",
+              }}
+            >
+              Frameworks & Libraries
+            </Typography>
+            {frameworks_libraries.map((fw: Skill) => (
+              <Typography key={fw.name} gutterBottom variant="body1">
+                {fw.name}
+              </Typography>
+            ))}
+          </Grid>
+          <Grid
+            item
+            display="flex"
+            flexDirection="column"
+            width={"230px"}
+            height={"180px"}
+          >
+            <Typography
+              gutterBottom
+              variant="h5"
+              sx={{
+                borderBottom: ".5px solid #D6A23A",
+              }}
+            >
               Testing
             </Typography>
-            <Divider />
             {testing.map((t: Skill) => (
               <Typography key={t.name} gutterBottom variant="body1">
                 {t.name}
               </Typography>
             ))}
           </Grid>
+          <Grid
+            item
+            display="flex"
+            flexDirection="column"
+            width={"230px"}
+            height={"180px"}
+          >
+            <Typography
+              gutterBottom
+              variant="h6"
+              sx={{
+                borderBottom: ".5px solid #D6A23A",
+              }}
+            >
+              Certifications
+            </Typography>
+            <Typography gutterBottom variant="body1">
+              {certifications[0].name}
+            </Typography>
+          </Grid>
         </Grid>
       </Stack>
+    </Stack>
   );
 };
